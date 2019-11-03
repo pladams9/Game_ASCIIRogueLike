0b3db9b1cf9839e1cc209df34d7e284287756ff5
Peter L. Adams <36312695+pladams9@users.noreply.github.com>
2019-11-03 12:40:02 -0600

    Cleaned up GameManager .h/.cpp

--- ASCIIRogueLike/GameManager.h
+++ ASCIIRogueLike/GameManager.h
@@ -1,17 +1,20 @@
-#pragma once
+#ifndef GAME_MANAGER_H
+#define GAME_MANAGER_H
+
+
+/* INCLUDES */
 #include <string>
-#include <conio.h>
 #include <vector>
 #include "Player.h"
 #include "Level.h"
 
-using std::string;
-using std::vector;
 
+/* CLASS DECLARATIONS */
 enum class GameStates
 {
 	MAIN_MENU, RESUME, SAVE, LOAD, HELP, EXIT
 };
+
 
 class GameManager
 {
@@ -19,12 +22,14 @@ class GameManager
 private:
 	Player _player;
 	Level _level;
-	vector<string> _mainMenu = { "***Main Menu***", "New Game", "Load Game", "Instructions", "Help", "Exit" };
+	std::vector<std::string> _mainMenu = { "***Main Menu***", "New Game", "Load Game", "Instructions", "Help", "Exit" };
 
 public:
-	GameManager(string levelFileName);
-	~GameManager();
+	GameManager(std::string levelFileName);
 	void runGame();
 	void playerInput();
 	void mainMenu();
 };
+
+
+#endif // GAME_MANAGER_H
